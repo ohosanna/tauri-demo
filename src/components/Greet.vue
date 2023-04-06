153c8479307e92b5e9fde899c3bf3ec174b46e57
chaos <ohosanna@gmail.com>
2023-04-06 16:03:36 +0800

    sidecar example

--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { invoke } from "@tauri-apps/api/tauri";
+import { Command } from '@tauri-apps/api/shell';
 
 const greetMsg = ref("");
 const name = ref("");
@@ -9,12 +10,21 @@ async function greet() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   greetMsg.value = await invoke("greet", { name: name.value });
 }
+async function sidecar() {
+  const command = Command.sidecar('binaries/hello', [
+    '-u',
+    name.value
+  ])
+  const output = await command.execute()
+  greetMsg.value = output.stdout.trim()
+}
 </script>
 
 <template>
   <div class="card">
     <input id="greet-input" v-model="name" placeholder="Enter a name..." />
     <button type="button" @click="greet()">Greet</button>
+    <button type="button" @click="sidecar()">Hello</button>
   </div>
 
   <p>{{ greetMsg }}</p>
